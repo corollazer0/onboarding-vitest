c0c8a7b6bb6a4c561d266a4ae4006c91ceed997f
corollazero <wovk1010@gmail.com>
2026-01-21 23:45:14 +0900

    09 🧩 feat: TaskList 컴포넌트 생성

---
+++ taskflow/src/components/TaskList.vue
@@ -0,0 +1,23 @@
+<script setup>
+import TaskItem from './TaskItem.vue'
+
+defineProps({
+  tasks: {
+    type: Array,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <section class="task-list">
+    <TaskItem v-for="task in tasks" :key="task.id" :task="task" />
+  </section>
+</template>
+
+<style scoped>
+.task-list {
+  display: grid;
+  gap: 12px;
+}
+</style>
