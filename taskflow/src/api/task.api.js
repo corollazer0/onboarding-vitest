eee9b1e882360e7c8cd5e5f1d7ae1a931ae98ae0
corollazero <wovk1010@gmail.com>
2026-01-22 08:13:12 +0900

    41 🔧 feat: taskApi 모듈 구현

---
+++ taskflow/src/api/task.api.js
@@ -0,0 +1,9 @@
+import apiClient from './client'
+
+export const taskApi = {
+  getAll: () => apiClient.get('/tasks'),
+  getById: (id) => apiClient.get(`/tasks/${id}`),
+  create: (data) => apiClient.post('/tasks', data),
+  update: (id, data) => apiClient.put(`/tasks/${id}`, data),
+  delete: (id) => apiClient.delete(`/tasks/${id}`)
+}
